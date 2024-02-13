b4001e3535372119bc7dce9abb0510f505e91fd4
neumann.niklas <neumann.niklas98@gmail.com>
2024-02-13 16:27:02 +0100

    docker, typeorm, database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeormConfigService } from './config/typeorm-config.service';
 
-@Module({})
+@Module({
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({ useClass: TypeormConfigService })
+    ]
+})
 export class AppModule { }
