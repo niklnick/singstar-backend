9ae1811667e498ed9bd83eb5e0fd5e0ef7d469cf
neumann.niklas <neumann.niklas98@gmail.com>
2024-04-08 20:49:02 +0200

    config refactor

--- src/config/typeorm-config.service.ts
+++ src/config/typeorm-config.service.ts
@@ -9,11 +9,11 @@ export class TypeormConfigService implements TypeOrmOptionsFactory {
     createTypeOrmOptions(): TypeOrmModuleOptions {
         return {
             type: 'postgres',
-            host: this.configService.get<string>('POSTGRES_HOST'),
-            port: this.configService.get<number>('POSTGRES_PORT'),
-            username: this.configService.get<string>('POSTGRES_USERNAME'),
-            password: this.configService.get<string>('POSTGRES_PASSWORD'),
-            database: this.configService.get<string>('POSTGRES_DATABASE'),
+            host: this.configService.get<string>('DATABASE_HOST'),
+            port: this.configService.get<number>('DATABASE_PORT'),
+            username: this.configService.get<string>('DATABASE_USERNAME'),
+            password: this.configService.get<string>('DATABASE_PASSWORD'),
+            database: this.configService.get<string>('DATABASE_DATABASE'),
             autoLoadEntities: true,
             synchronize: true
         }
